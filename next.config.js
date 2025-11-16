9cfdbe747d8d1eac8c3a9222ba08181815f68181
杨洋 <yy@yangyangdeMacBook-Air.local>
2025-11-16 13:46:36 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 设置基础路径，访问 https://domain.com/OmxoUR 才展示页面
+  basePath: '/OmxoUR',
   // 支持 ES modules
   experimental: {
     serverActions: {
